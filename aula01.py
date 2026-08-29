6659d3a7e16c9f30774008ee38f2cce379adf2f6
paulinhocristianosantos-byte <paulinhocristianosantos@gmail.com>
2026-08-28 21:01:55 -0300

    ajustes

--- aula01.py
+++ aula01.py
@@ -5,19 +5,19 @@ curso = "Analista de Dados"
 media_nota = 8.5
 ativo = True 
 # exibindo os valores 
-print (curso, alunos, ativo)
+print(curso, alunos, ativo)
 
-print ("Digite um valor:")
+print("Digite um valor:")
 teste = input()
 print(type(teste))
 dobro = int(teste) *2
-print ("Resultado:", dobro)
+print("Resultado:", dobro)
 
 idade = int(input("Sua idade:"))
 print(type(idade))
-print ("Sua idade é:", idade)
+print("Sua idade é:", idade)
 # agora idade é um numero inteiro!
 
-Codigo exercicio_aula pv:
-nome_produto - input ("Digite o nome do produto")
+#Codigo exercicio_aula pv:
+nome_produto = input("Digite o nome do produto")
 valor_produto = float(input("Digite o valor do produto:"))
